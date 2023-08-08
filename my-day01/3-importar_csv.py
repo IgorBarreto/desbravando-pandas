e3381f78c4ab91bd29140d1465e83e33b1d23309
Igor Barreto <igorbarreto1993@hotmail.com>
2023-08-08 00:25:12 -0300

    mend

--- my-day01/3-importar_csv.py
+++ my-day01/3-importar_csv.py
@@ -1,12 +1,9 @@
 # %%
 import warnings
 import pandas as pd
-
-
 warnings.filterwarnings('ignore')
-
 # %%
-df = pd.read_csv('data/pedido.csv')
+df = pd.read_csv('../data/pedido.csv')
 
 # %%
 df.head()
